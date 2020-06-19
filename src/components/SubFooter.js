78bca2e103146ef8c0b4cd3a1f5c00be5304b0ba
Avaneesh <mavaneesh@gmail.com>
2020-06-19 18:10:19 -0400

    Updated Footer implementation and added title

--- src/components/SubFooter.js
+++ src/components/SubFooter.js
@@ -9,20 +9,25 @@ import { Link } from 'gatsby';
 
 const SubFooter = props => (
   <div className="sub-footer-strip">
-     <div className="img1">
-            <img alt="logo" src={logo} />
-    </div>
+
     <div className="container">
+
+    <div className="img1">
+            <img alt="logo" src={logo} />
+          </div>
    
-
+    <div className="linkedin">
             <a href="https://www.linkedin.com/company/software-for-love/" target="_blank"><img alt="Linkedin" src={linkedin} /></a>
-
+            </div>
+    <div className="twitter">
             <a href="https://twitter.com/softwareforlove" target="_blank"><img alt="Twitter" src={twitter} /></a>
-
+            </div>
+    <div className="facebook">
             <a href="https://m.facebook.com/softwareforlove/" target="_blank"><img alt="Facebook" src={facebook} /></a>
-
+            </div>
+     <div className="mail">
             <a href="mailto:team@softwareforlove.com" target="_blank"><img alt="Gmail" src={gmail} /></a>
-        
+            </div>
     </div>
   </div>
 );
