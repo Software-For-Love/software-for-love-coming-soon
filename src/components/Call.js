001d91cd5e0720519d50b0948867dfbe8ae648a4
kandloic <jkand011@uottawa.ca>
2020-06-25 18:36:28 -0400

    implemented email validation function, causes error

--- src/components/Call.js
+++ src/components/Call.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
+var emailFeedBack = "";
+
+/* function validateEmail(){
+  var text = document.querySelector("email-input");
+  var regx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  console.log(text); // debuging
+  if (regx.test(text)){
+    document.querySelector("email-input").style.borderBottom="1px solid green";
+    emailFeedBack = "Thank you for signing up!";
+  } else {
+    document.querySelector("email-input").style.borderBottom="1px solid red";
+    emailFeedBack = "Email Invalid. Please try again";
+    console.log("false");
+  }
+} */
+
 const Call = props => (
   <div className="call">
     <div className="call-box-top">
@@ -15,13 +31,25 @@ const Call = props => (
         <a href={`mailto:${props.data.site.siteMetadata.contact.email}`}>
           {props.data.site.siteMetadata.contact.email}
         </a> */}
-        <strong><label for="email">Email :</label></strong>
-        <input type="email" class="email-input" name="email" placeholder="Enter Your Email"></input>
+        <strong><label htmlFor="email">Email :</label></strong>
+        <input type="email"
+        className="email-input"
+        name="email"
+        placeholder="Enter Your Email"
+        autoComplete="off"
+        style={{borderBottom : "1px solid red"}}
+        />
+        <div id="Invalid Email"
+        style={ {color: "red"}, {fontSize: 10}}
+        >{emailFeedBack}</div>
       </div>
     </div>
     {props.button && (
       <div className="call-box-bottom">
-        <a className="button">
+        <a 
+        className="button"
+        type="submit"
+        /* onClick={validateEmail()} */>
           Submit
         </a>
       </div>
