0bd2c65619fdd097d7cf2f066470616ac357faea
kandloic <jkand011@uottawa.ca>
2020-06-29 22:51:58 -0400

    changed website title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const guid = process.env.NETLIFY_GOOGLE_ANALYTICS_ID;
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Serif',
+    title: 'Software For Love',
     description: 'my theme',
     contact: {
       phone: 'XXX XXX XXX',
