8ae4b612acf269adc46287e520e12caa9588a926
kandloic <jkand011@uottawa.ca>
2020-07-02 16:02:22 -0400

    added handleSubmit function back and made some fixes in subfooter.js

--- src/components/Call.js
+++ src/components/Call.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { StaticQuery, graphql, navigate} from 'gatsby';
 
 var feedback = "Thank you for signing up!"
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+function handleSubmit(e){
+  e.preventDefault()
+  const form = e.target
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: encode({
+      'form-name': form.getAttribute('name')
+    }),
+  })
+    .then(() => navigate(form.getAttribute('action')))
+    .catch((error) => alert(error))
+}
 
 /* function textAppear() {
   var text = document.getElementById("feedback-message");
